f3fbc7d6d35cdd3e7de4986b2ec2cf9b1d46870c
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-02-17 14:17:44 +0900

    Initial Commit

---
+++ LastfmClient/LastfmClient.h
@@ -0,0 +1,19 @@
+//
+//  LastfmClient.h
+//  LastfmClient
+//
+//  Created by kensuke-hoshikawa on 2018/02/17.
+//  Copyright © 2018年 star__hoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LastfmClient.
+FOUNDATION_EXPORT double LastfmClientVersionNumber;
+
+//! Project version string for LastfmClient.
+FOUNDATION_EXPORT const unsigned char LastfmClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LastfmClient/PublicHeader.h>
+
+

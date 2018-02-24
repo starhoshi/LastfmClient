20b90600954f2aff180a48ee2743605ea318d701
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-02-25 01:36:01 +0900

    recent tracks api

--- LastfmClient/Entity/RecentTrack.swift
+++ LastfmClient/Entity/RecentTrack.swift
@@ -44,12 +44,13 @@ public struct Attr: Decodable {
 public struct Track: Decodable {
     public let name: String
     public let image: Image
-    public let loved: Bool
+    public let loved: Bool?
     public let streamable: Bool
     public let mbid: String
     public let url: URL
     public let date: Date
     public let album: Album
+    public let artist: Artist
 
     private enum CodingKeys: String, CodingKey {
         case name
@@ -60,6 +61,7 @@ public struct Track: Decodable {
         case url
         case date
         case album
+        case artist
     }
 
     public init(from decoder: Decoder) throws {
@@ -67,12 +69,14 @@ public struct Track: Decodable {
 
         name = try decoder.decode(String.self, forKey: .name)
         image = try decoder.decode(ImageDecodableMap.self, forKey: .image).decoded
-        loved = try decoder.decode(StringCodableMap<Int>.self, forKey: .loved).decoded == 1
+        let loved = try decoder.decodeIfPresent(StringCodableMap<Int>.self, forKey: .loved)?.decoded
+        self.loved = loved == nil ? nil : loved == 1
         streamable = try decoder.decode(StringCodableMap<Int>.self, forKey: .streamable).decoded == 1
         mbid = try decoder.decode(String.self, forKey: .mbid)
         url = try decoder.decode(URL.self, forKey: .url)
         date = try decoder.decode(DateDecodableMap.self, forKey: .date).decoded
         album = try decoder.decode(Album.self, forKey: .album)
+        artist = try decoder.decode(Artist.self, forKey: .artist)
     }
 }
 
@@ -86,3 +90,35 @@ public struct Album: Decodable {
     }
 }
 
+public struct Artist: Decodable {
+    public let name: String
+    private let text: String? = nil
+    public let mbid: String
+    public let url: URL?
+    public let image: Image?
+
+    private enum CodingKeys: String, CodingKey {
+        case name
+        case text = "#text"
+        case mbid
+        case url
+        case image
+    }
+
+    public init(from decoder: Decoder) throws {
+        let decoder = try decoder.container(keyedBy: CodingKeys.self)
+
+        let name = try decoder.decodeIfPresent(String.self, forKey: .name)
+        let text = try decoder.decodeIfPresent(String.self, forKey: .text)
+        if let name = name {
+            self.name = name
+        } else if let text = text {
+            self.name = text
+        } else {
+            throw DecodingError.valueNotFound(Artist.self, DecodingError.Context(codingPath: [CodingKeys.name], debugDescription: "Both name and #text are nil."))
+        }
+        image = try decoder.decodeIfPresent(ImageDecodableMap.self, forKey: .image)?.decoded
+        mbid = try decoder.decode(String.self, forKey: .mbid)
+        url = try decoder.decodeIfPresent(URL.self, forKey: .url)
+    }
+}
--- LastfmClientTests/UserTests.swift
+++ LastfmClientTests/UserTests.swift
@@ -44,11 +44,11 @@ class UserTests: XCTestCase {
             switch result {
             case .success(let user):
                 XCTAssertEqual(user.name, "RJ")
-                XCTAssertEqual(user.realname!, "Richard Jones ")
-                XCTAssertEqual(user.image.small!.absoluteString, "https://lastfm-img2.akamaized.net/i/u/34s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
-                XCTAssertEqual(user.image.medium!.absoluteString, "https://lastfm-img2.akamaized.net/i/u/64s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
-                XCTAssertEqual(user.image.large!.absoluteString, "https://lastfm-img2.akamaized.net/i/u/174s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
-                XCTAssertEqual(user.image.extralarge!.absoluteString, "https://lastfm-img2.akamaized.net/i/u/300x300/b26d6fd11de240a1c045dfb5c5d9fe65.png")
+                XCTAssertEqual(user.realname, "Richard Jones ")
+                XCTAssertEqual(user.image.small?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/34s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
+                XCTAssertEqual(user.image.medium?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/64s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
+                XCTAssertEqual(user.image.large?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/174s/b26d6fd11de240a1c045dfb5c5d9fe65.png")
+                XCTAssertEqual(user.image.extralarge?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/300x300/b26d6fd11de240a1c045dfb5c5d9fe65.png")
             case .failure(let error):
                 XCTFail("\(error)")
             }
@@ -58,7 +58,7 @@ class UserTests: XCTestCase {
         wait(for: [expectation], timeout: 20)
     }
 
-    func testGetRecentTracks() {
+    func testGetRecentTracksWhenExtendedTrue() {
         let expectation = XCTestExpectation(description: "getRecentTracks")
 
         let user = UserAPI(user: "star__hoshi")
@@ -76,12 +76,56 @@ class UserTests: XCTestCase {
                 XCTAssertNil(user.list[0].image.medium)
                 XCTAssertNil(user.list[0].image.large)
                 XCTAssertNil(user.list[0].image.extralarge)
-                XCTAssertFalse(user.list[0].loved)
+                XCTAssertFalse(user.list[0].loved!)
                 XCTAssertFalse(user.list[0].streamable)
                 XCTAssertEqual(user.list[0].url.absoluteString, "https://www.last.fm/music/U/_/little+my+star")
                 XCTAssertEqual(user.list[0].date.timeIntervalSince1970, 1519135099)
                 XCTAssertEqual(user.list[0].album.text, "魔界天使ジブリール２(フロントウイング)")
                 XCTAssertEqual(user.list[0].album.mbid, "")
+                XCTAssertEqual(user.list[0].artist.name, "U")
+                XCTAssertEqual(user.list[0].artist.mbid, "f068f7a7-8cff-4104-b7a4-60cc2a060f5a")
+                XCTAssertEqual(user.list[0].artist.url?.absoluteString, "https://www.last.fm/music/U")
+                XCTAssertEqual(user.list[0].artist.image?.small?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/34s/7c22d3dacc4e4a39b24e8d483d5d0e5a.png")
+                XCTAssertEqual(user.list[0].artist.image?.medium?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/64s/7c22d3dacc4e4a39b24e8d483d5d0e5a.png")
+                XCTAssertEqual(user.list[0].artist.image?.large?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/174s/7c22d3dacc4e4a39b24e8d483d5d0e5a.png")
+                XCTAssertEqual(user.list[0].artist.image?.extralarge?.absoluteString, "https://lastfm-img2.akamaized.net/i/u/300x300/7c22d3dacc4e4a39b24e8d483d5d0e5a.png")
+            case .failure(let error):
+                XCTFail("\(error)")
+            }
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 20)
+    }
+
+    func testGetRecentTracksWhenExtendedFalse() {
+        let expectation = XCTestExpectation(description: "getRecentTracks")
+
+        let user = UserAPI(user: "star__hoshi")
+        user.getRecentTracks(limit: 100, from: 1519052626, to: 1519139026, extended: false) { result in
+            switch result {
+            case .success(let user):
+                XCTAssertEqual(user.attr.user, "star__hoshi")
+                XCTAssertEqual(user.attr.page, 1)
+                XCTAssertEqual(user.attr.perPage, 100)
+                XCTAssertEqual(user.attr.total, 15)
+                XCTAssertEqual(user.attr.totalPages, 1)
+                XCTAssertEqual(user.list.count, 15)
+                XCTAssertEqual(user.list[0].name, "little my star")
+                XCTAssertNil(user.list[0].image.small)
+                XCTAssertNil(user.list[0].image.medium)
+                XCTAssertNil(user.list[0].image.large)
+                XCTAssertNil(user.list[0].image.extralarge)
+                XCTAssertNil(user.list[0].loved)
+                XCTAssertFalse(user.list[0].streamable)
+                XCTAssertEqual(user.list[0].url.absoluteString, "https://www.last.fm/music/U/_/little+my+star")
+                XCTAssertEqual(user.list[0].date.timeIntervalSince1970, 1519135099)
+                XCTAssertEqual(user.list[0].album.text, "魔界天使ジブリール２(フロントウイング)")
+                XCTAssertEqual(user.list[0].album.mbid, "")
+                XCTAssertEqual(user.list[0].artist.name, "U")
+                XCTAssertEqual(user.list[0].artist.mbid, "f068f7a7-8cff-4104-b7a4-60cc2a060f5a")
+                XCTAssertNil(user.list[0].artist.url?.absoluteString)
+                XCTAssertNil(user.list[0].artist.image)
             case .failure(let error):
                 XCTFail("\(error)")
             }
